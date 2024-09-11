0aeb705f75a6b3d65a00bfa4aae6b11c5592946d
Gregorio Patriota <gpc.cin@gmail.com>
2024-09-11 06:28:13 -0300

    WIP - trying to decrypt the file and read it

--- CesarCipher/Program.cs
+++ CesarCipher/Program.cs
@@ -1,4 +1,7 @@
-﻿string input = @"Ibpê yrh fboer nf Pvsenf qr Péfne? Rfcreb dhr fvz! Fr ibpê purtbh ngé ndhv é cbedhr fbhor hfne b EBG13 pregvaub. Znf pnyzn crdhrab tnsnaubgb dhr nvaqn aãb npnobh. Ibpê cerpvfn ntben hgvyvmne hz nytbegvzb qr punir fvzégevpn cnen qrpevcgne n zrafntrz dhr rfgá ab nedhvib narkb! Dhr gny hgvyvmne hz nytbegvzb cnqeãb vagreanpvbany qr pevcgbtensvn cbe oybpbf qr 256 ovgf: b cnqeãb qr pevcgbtensvn ninaçnqn qr 256 ovgf. Pbzb ibpê aãb é ivqragr, ibh gr qne hzn pbyure qr puá, nabgn n punir nv: CSN_FvQv_20NABF (frz nf nfcnf).";
+﻿using System.Security.Cryptography;
+using System.Text;
+
+string input = @"Ibpê yrh fboer nf Pvsenf qr Péfne? Rfcreb dhr fvz! Fr ibpê purtbh ngé ndhv é cbedhr fbhor hfne b EBG13 pregvaub. Znf pnyzn crdhrab tnsnaubgb dhr nvaqn aãb npnobh. Ibpê cerpvfn ntben hgvyvmne hz nytbegvzb qr punir fvzégevpn cnen qrpevcgne n zrafntrz dhr rfgá ab nedhvib narkb! Dhr gny hgvyvmne hz nytbegvzb cnqeãb vagreanpvbany qr pevcgbtensvn cbe oybpbf qr 256 ovgf: b cnqeãb qr pevcgbtensvn ninaçnqn qr 256 ovgf. Pbzb ibpê aãb é ivqragr, ibh gr qne hzn pbyure qr puá, nabgn n punir nv: CSN_FvQv_20NABF (frz nf nfcnf).";
 int rot13 = 13;
 
 int upperFirstLetter = 65;
@@ -25,3 +28,29 @@ foreach(char l in input) {
 }
 
 Console.WriteLine(output);
+
+string encoded = string.Empty;
+string aesKey = string.Empty;
+// string plaintext = string.Empty;
+
+using(StreamReader st = new StreamReader("enc.txt", Encoding.UTF8)) {
+    encoded = st.ReadToEnd();
+    using(Aes aes = Aes.Create()) {
+        aes.Key = Encoding.UTF8.GetBytes(aesKey);
+        // aes.IV = Encoding.UTF8.GetBytes(encoded);
+
+        ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+        using (MemoryStream msDecrypt = new MemoryStream(Encoding.UTF8.GetBytes(encoded)))
+        {
+            using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+            {
+                using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                {
+                    // plaintext = srDecrypt.ReadToEnd();
+                    Console.WriteLine(srDecrypt.ReadToEnd());
+                }
+            }
+        }
+    }
+}
