a7b5e34186bf4d3cbe1ec541181584d83f9e260c
Gregorio Patriota <gpc.cin@gmail.com>
2024-09-11 06:11:36 -0300

    Removing unused variable, this source code is just for fun

--- CesarCipher/Program.cs
+++ CesarCipher/Program.cs
@@ -1,12 +1,11 @@
 ﻿string input = @"Ibpê yrh fboer nf Pvsenf qr Péfne? Rfcreb dhr fvz! Fr ibpê purtbh ngé ndhv é cbedhr fbhor hfne b EBG13 pregvaub. Znf pnyzn crdhrab tnsnaubgb dhr nvaqn aãb npnobh. Ibpê cerpvfn ntben hgvyvmne hz nytbegvzb qr punir fvzégevpn cnen qrpevcgne n zrafntrz dhr rfgá ab nedhvib narkb! Dhr gny hgvyvmne hz nytbegvzb cnqeãb vagreanpvbany qr pevcgbtensvn cbe oybpbf qr 256 ovgf: b cnqeãb qr pevcgbtensvn ninaçnqn qr 256 ovgf. Pbzb ibpê aãb é ivqragr, ibh gr qne hzn pbyure qr puá, nabgn n punir nv: CSN_FvQv_20NABF (frz nf nfcnf).";
 int rot13 = 13;
+
 int upperFirstLetter = 65;
 int upperLastLetter = 90;
-int upperRange = upperLastLetter - upperFirstLetter;
 
 int lowerFirstLetter = 97;
 int lowerLastLetter = 122;
-int lowerRange = lowerLastLetter - lowerFirstLetter;
 
 string output = string.Empty;
 
